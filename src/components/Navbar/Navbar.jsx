5887bfd23d8f720961bce2309aece3c1b2149afb
Arif1917 <mdarifhosen1917@gmail.com>
2025-04-22 20:51:55 +0600

    lift up and down added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,21 +10,29 @@ const dataNavigation = [
 const Navbar = () => {
 
     const [open, setOpen] = useState(false)
+    const links =  dataNavigation.map(data => <li key={data.id} className='md:mr-10 px-4 hover:bg-gray-400'><a href={data.path}>{data.name}</a></li>)
+    
 
     return (
         <div>
             <nav className='flex items-center justify-between px-10 py-4'>
+                
 
                <span onClick={()=> setOpen(!open)} className='flex mr-10'>
                 { open ? <X size={45} color='black' className='md:hidden'></X> : <Menu size={45} color='black' className='md:hidden'/>}
-               
-              
+                <ul className={`md:hidden absolute ${open ? 'top-16' : '-top-40'} duration-700 bg-amber-400`}>
+                {
+                    links
+                }
+              </ul>
+                <h2 className='text-3xl font-medium'>Navbar</h2>
                </span>
-               <h2 className='text-3xl font-medium'>Navbar</h2>
-                <ul className='flex'>
-                    {
-                        dataNavigation.map(data => <li key={data.id} className='mr-10'><a href={data.path}>{data.name}</a></li>)
-                    }
+              
+            
+                <ul className='md:flex hidden'>
+                   {
+                    links
+                   }
                 </ul>
 
                 <button className='text-2xl text-black bg-amber-500 rounded-xl px-4 py-2'>Sign In</button>
